df6c2a8f74b1a68c4c97a3833225aa912527c434
Anh Le <letuananh.ttmmt.ctu@gmail.com>
2020-11-05 18:37:42 +0700

    Add simple helloworld example

--- Makefile
+++ Makefile
@@ -3,3 +3,18 @@ run-server:
 
 run-client:
 	go run cmd/client/main.go
+
+tidy:
+	go mod tidy
+	go mod vendor
+	go install \
+		github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway \
+		github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2 \
+		google.golang.org/protobuf/cmd/protoc-gen-go \
+		google.golang.org/grpc/cmd/protoc-gen-go-grpc
+
+gen:
+	protoc -I . \
+	--go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    api/proto/helloworld/*.proto
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/captain-corgi/grpc-go-example/api/proto/helloworld"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+type (
+	//server to implement helloworld.GreeterServer
+	server struct {
+		helloworld.UnimplementedGreeterServer
+	}
+)
+
+func (r *server) SayHello(ctx context.Context, request *helloworld.HelloRequest) (reply *helloworld.HelloReply, err error) {
+	log.Printf("Received: %v", request.GetName())
+	return &helloworld.HelloReply{
+		Message: fmt.Sprintf("Hello %s", request.GetName()),
+	}, nil
+}
 
 func main() {
-	fmt.Println("Hello gRPC server")
+	// Define server
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	sv := grpc.NewServer()
+
+	// Register service
+	helloworld.RegisterGreeterServer(sv, &server{})
+
+	// Start server
+	log.Printf("Server started in port %s\n", port)
+	log.Fatalf("failed to start server %v", sv.Serve(lis))
 }
